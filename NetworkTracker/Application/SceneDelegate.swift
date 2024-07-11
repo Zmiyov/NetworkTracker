7c603588f1efb75ae2ddd2d173cbda89afe0f067
Volodymyr <zmiyov88@gmail.com>
2024-07-11 13:52:26 +0300

    First commit with basic setup

---
+++ NetworkTracker/Application/SceneDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  SceneDelegate.swift
+//  NetworkTracker
+//
+//  Created by Volodymyr Pysarenko on 11.07.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
+
+        let viewController = MainViewController()
+        let navController = UINavigationController(rootViewController: viewController)
+        
+        window.rootViewController = navController
+        window.makeKeyAndVisible()
+    }
+
+}
+
