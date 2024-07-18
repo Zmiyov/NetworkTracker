1dc26b7cb1fbcf0e29d6570271491e1935368d16
Volodymyr <zmiyov88@gmail.com>
2024-07-18 13:19:57 +0300

    Icon is added for sending to test flight

--- NetworkTracker/Common/Constants.swift
+++ NetworkTracker/Common/Constants.swift
@@ -12,12 +12,10 @@ import Foundation
 ///     - requestFilter: A String representing the domain to filter network requests
 ///     - appGroupIdentifier:  A String that used as a group identifier in the whole app
 ///     - notificationCategory: A String defining the notification category identifier
-///     - pushActivityKey: A String key used for push activity notifications
 struct Constants {
     static let requestFilter = "google.com"
     static let appGroupIdentifier = "group.com.pysarenkodev.NetworkTracker"
     static let notificationCategory = "network_request_category"
-    static let pushActivityKey = "push_activity_key"
     
     /// Enum containing name for ObservableNotification that is used for tracking changes in Core Data
     enum ObservableNotification {
